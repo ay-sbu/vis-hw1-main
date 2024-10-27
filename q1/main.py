cf2fb16a808ecc1513efa84f1224fbc5df0f4a3a
abbasyazdanmehr <kjkj55047@gmail.com>
2024-10-27 19:26:57 +0330

    feat: add q1 solution

---
+++ q1/main.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageEnhance, ImageOps
+from pytesseract import pytesseract 
+import cv2
+import numpy as np
+
+def grayscaler():
+    img = Image.open("figs/car.jpg")
+    img = img.resize((img.width * 15, img.height * 15))
+    img = img.convert('L')
+    img = img.point(lambda x: 0 if x < 50 else 255, '1')
+    img.save("figs/enhanced.jpg")
+    
+def extract_text():
+    img = Image.open("figs/enhanced.jpg") 
+
+    text = pytesseract.image_to_string(img) 
+
+    print("text found: ", text[:-1])
+
+grayscaler()
+extract_text()
