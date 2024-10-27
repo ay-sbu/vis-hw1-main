0b168fa1d3f1292f8a1e2abb44fe336a51bb85c9
abbasyazdanmehr <kjkj55047@gmail.com>
2024-10-27 19:35:00 +0330

    fix: improve naming

--- q1/main.py
+++ q1/main.py
@@ -8,10 +8,10 @@ def grayscaler():
     img = img.resize((img.width * 15, img.height * 15))
     img = img.convert('L')
     img = img.point(lambda x: 0 if x < 50 else 255, '1')
-    img.save("figs/enhanced.jpg")
+    img.save("figs/enhanced-car.jpg")
     
 def extract_text():
-    img = Image.open("figs/enhanced.jpg") 
+    img = Image.open("figs/enhanced-car.jpg") 
 
     text = pytesseract.image_to_string(img) 
 
